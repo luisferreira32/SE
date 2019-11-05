b825e6a77f461db9d97dad6841524f09e1077e60
luisferreira32 <luis.pmm.ferreira@gmail.com>
2019-11-05 07:40:35 +0000

    templates settings changed

--- doggag/doggag/urls.py
+++ doggag/doggag/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 urlpatterns = [
     path('', include('apps.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
